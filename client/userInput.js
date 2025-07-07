7aa77dae1e9b26dbfe7421c8d1ec17e39223aa85
שם המשתמש שלך <your-email@example.com>
2025-07-07 22:21:51 +0300

    add update and delete riddles functions

--- client/userInput.js
+++ client/userInput.js
@@ -1,3 +1,5 @@
+import readline from 'readline-sync';
+
 export function riddlesDetails(){
     const difficulty = readline.question("Enter the difficulty(easy/medium/hard): ");
     const timeLimit = readline.question("Enter the time limit: ");
@@ -18,6 +20,91 @@ export function riddlesDetails(){
 
 }
 
-function riddleMenu(){
-    console.log("1. add riddle")
+export function updateRiddleOptions(){
+    const id = readline.question("Enter the id of the riddle you want to update: ");
+
+    const question = readline.question("which field you want to update?\n1: all\n2: name\n3: description\n4: correctAnswer\n5: difficulty\n6: timeLimit\n7: hint\n");
+    
+    let newRiddle = {}
+    let fullRiddle = {}
+    let riddleName, riddleDescription, correctAnswer, difficulty, timeLimit, hint;
+    
+    switch(question){
+        case "1":
+            riddleName = readline.question("Enter the new riddle name: ");
+            riddleDescription = readline.question("Enter the new riddle description: ");
+            correctAnswer = readline.question("Enter the new correct answer: ");
+            difficulty = readline.question("Enter the new difficulty: ");
+            timeLimit = readline.question("Enter the new time limit: ");
+            hint = readline.question("Enter the new hint: ");
+
+            fullRiddle = {
+                id: parseInt(id),
+                name: riddleName,
+                description: riddleDescription,
+                correctAnswer: correctAnswer,
+                difficulty: difficulty,
+                timeLimit: timeLimit,
+                hint: hint
+            }
+            return fullRiddle;
+            break;
+        case "2":
+            riddleName = readline.question("Enter the new riddle name: ");
+            newRiddle = {
+                id: id,
+                name: riddleName,
+            }
+            return newRiddle;
+            break;
+        case "3":
+            riddleDescription = readline.question("Enter the new riddle description: ");
+            newRiddle = {
+                id: id,
+                description: riddleDescription,
+            }
+            return newRiddle;
+            break;
+        case "4":
+            correctAnswer = readline.question("Enter the new correct answer: ");
+            newRiddle = {
+                id: id,
+                correctAnswer: correctAnswer,
+            }
+            return newRiddle;
+            break;
+        case "5":
+            difficulty = readline.question("Enter the new difficulty: ");
+            newRiddle = {
+                id: id,
+                difficulty: difficulty,
+            }   
+            return newRiddle;
+            break;
+        case "6":
+            timeLimit = readline.question("Enter the new time limit: ");
+            newRiddle = {
+                id: id,
+                timeLimit: timeLimit,
+            }
+            return newRiddle;
+            break;
+        case "7":
+            hint = readline.question("Enter the new hint: ");
+            newRiddle = {
+                id: id,
+                hint: hint,
+            }
+            return newRiddle;
+            break;
+        default:
+            console.log("Invalid option");
+            break;
+    }
+}
+
+
+export function AskForRiddleID(){
+    const id = readline.question("Enter the id of the riddle you want to update: ");
+    return id;
 }
--- server/services/riddles.service.js
+++ server/services/riddles.service.js
@@ -1,4 +1,5 @@
 import dal from '../dal/index.dal.js'
+import { updateRiddleOptions } from '../../client/userInput.js'
 import { riddlesDetails } from '../../client/userInput.js'
 
 async function createRiddle(){
@@ -43,3 +44,76 @@ async function readRiddle(){
     }
 }
 
+
+async function updateRiddle(id){
+    const riddle = updateRiddleOptions(id);
+    const riddles = await dal.riddles.GetAll()
+
+    const index = riddles.findIndex(r => parseInt(r.id) === parseInt(id));
+
+    if(index !== -1){
+        if (riddle.length > 2){
+        riddles[index] = riddle;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+        
+    }else if(riddle.name !== undefined){
+        riddles[index].name = riddle.name;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+
+    }else if(riddle.description !== undefined){
+        riddles[index].description = riddle.description;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+
+    }else if(riddle.correctAnswer !== undefined){
+        riddles[index].correctAnswer = riddle.correctAnswer;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+
+    }else if(riddle.difficulty !== undefined){
+        riddles[index].difficulty = riddle.difficulty;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+
+    }else if(riddle.timeLimit !== undefined){
+        riddles[index].timeLimit = riddle.timeLimit;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+
+    }else if(riddle.hint !== undefined){
+        riddles[index].hint = riddle.hint;
+        await dal.riddles.Write(riddles);
+        console.log("riddle updated successfully");
+
+    }
+    }else{
+    console.log("riddle not found");
+    return 
+    }
+}
+
+async function deleteRiddle(){
+    const riddleID = AskForRiddleID();
+try{
+    const riddles = await dal.riddles.GetAll()
+
+    const index = riddles.findIndex(r => r.id === parseInt(riddleID));
+
+    if(index !== -1){
+        riddles.splice(index, 1);
+        await dal.riddles.Write(riddles);
+        console.log("riddle deleted successfully");
+    }else{
+        console.log("riddle not found");
+        return 
+    }
+
+}catch(err){
+    console.log(err);
+    return err;
+}
+}
+
+export { createRiddle, readRiddle, updateRiddle, deleteRiddle }
